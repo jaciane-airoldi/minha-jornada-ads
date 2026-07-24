1762056b8c94ef9d7f9e38d28dfb455d095e1c6b
jaciane <airoldijaciane@icloud.com>
2026-07-23 21:38:42 -0300

    3 primeiro commit meu primiro codigo em python

---
+++ primeiro-codigo.py
@@ -0,0 +1,4 @@
+print("Olá, mundo! Eu sou a Jaciane e estou começando minha jornada em tecnologia!")
+
+
+## Este é o meu primeiro código em Python. Estou animada para aprender mais e desenvolver minhas habilidades em programação.
